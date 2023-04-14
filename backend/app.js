a1959fa5ffb938eeb883d8570a21a23f2bbfe4c3
LuuDucKhanh2022 <luuduckhanharsenal@.com>
2023-04-14 14:48:03 +0700

    enable static file

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const enableRedisClient = require("./config/redis");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(`public`));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 connectDatabase();
 
